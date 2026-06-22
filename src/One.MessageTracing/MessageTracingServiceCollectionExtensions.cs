aa21189a7d48d717658c9b1bcc6a370bee3ada06
Kalina Shupeva <kalina.shupeva@eldersoss.com>
2026-06-22 14:36:15 +0300

    fix: register tracers in multiple assemblies

--- src/One.MessageTracing/MessageTracingServiceCollectionExtensions.cs
+++ src/One.MessageTracing/MessageTracingServiceCollectionExtensions.cs
@@ -5,18 +5,30 @@ namespace One.MessageTracing;
 
 public static class MessageTracingServiceCollectionExtensions
 {
-    public static IServiceCollection AddMessageTracing(this IServiceCollection services, Assembly assembly)
+    public static IServiceCollection AddMessageTracing(this IServiceCollection services, IEnumerable<Assembly> assemblies)
     {
-        IEnumerable<Type> tracers = assembly.GetTypes().Where(t => typeof(IMessageTracer).IsAssignableFrom(t) && t.IsInterface == false && t.IsAbstract == false);
+        IEnumerable<Type> tracers = assemblies.SelectMany(x => x.GetLoadableTypes())
+            .Where(t => typeof(IMessageTracer).IsAssignableFrom(t) && t.IsInterface == false && t.IsAbstract == false);
+
         foreach (var type in tracers)
             services.AddSingleton(typeof(IMessageTracer), type);
 
-        IEnumerable<Type> traceWriters = assembly.GetTypes().Where(t => typeof(IMessageTraceWriter).IsAssignableFrom(t) && t.IsInterface == false && t.IsAbstract == false);
+        IEnumerable<Type> traceWriters = assemblies.SelectMany(x => x.GetLoadableTypes())
+            .Where(t => typeof(IMessageTraceWriter).IsAssignableFrom(t) && t.IsInterface == false && t.IsAbstract == false);
+
         foreach (var type in traceWriters)
             services.AddSingleton(typeof(IMessageTraceWriter), type);
 
         services.AddSingleton<MessageTracer>();
 
         return services;
+    }
+
+    static IEnumerable<Type> GetLoadableTypes(this Assembly assembly)
+    {
+        if (assembly is null) throw new ArgumentNullException(nameof(assembly));
+
+        try { return assembly.GetTypes(); }
+        catch (ReflectionTypeLoadException e) { return e.Types.Where(t => t != null); }
     }
 }

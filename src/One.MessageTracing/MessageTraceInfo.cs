aef7b98fe140949a4c036bae23f51e799d31feb1
Kalina Shupeva <kalina.shupeva@eldersoss.com>
2026-06-25 12:55:50 +0300

    feat: Add the tenant in the MessageTraceInfo

--- src/One.MessageTracing/MessageTraceInfo.cs
+++ src/One.MessageTracing/MessageTraceInfo.cs
@@ -2,14 +2,16 @@
 
 public record class MessageTraceInfo
 {
-    public MessageTraceInfo(string messageId, string causationId, string correlationId)
+    public MessageTraceInfo(string messageId, string causationId, string correlationId, string tenant)
     {
         MessageId = messageId;
         CausationId = causationId;
         CorrelationId = correlationId;
+        Tenant = tenant;
     }
 
     public string MessageId { get; private set; }
     public string CausationId { get; private set; }
     public string CorrelationId { get; private set; }
+    public string Tenant { get; private set; }
 }
